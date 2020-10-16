410a3ce37f9c5dbd1411592212bf8d0ba69369fd
Subin Min <tnqls5417@naver.com>
2020-10-16 10:36:00 +0900

    multi

---
+++ multi.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 8; i++) {//9단까지 하려면 8 -> 9로 수정
+        for (int j = 1; j <= 9; j++) {
+            cout << i << " * " << j << " = " << i * j << "\n";
+        }
+    }
+    return 0;
+}
